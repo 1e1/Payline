d2a4c44098e321c0e43b71dbf5d1d329ccecdbfa
aglr <aymeric.gerlier@rentacar.fr>
2016-03-22 14:53:03 +0100

    initial commit

---
+++ Payline.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AyGLR
+ * Date: 22/03/16
+ * Time: 14:31
+ */
+
+
+namespace Payline {
+
+    require __DIR__ . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'paylineSDK.php';
+
+    class Payline extends \paylineSDK {}
+    /*
+    class Util extends \paylineUtil{}
+    class Payment extends \pl_payment{}
+    class Order extends \pl_order{}
+    class PrivateData extends \pl_privateData{}
+    class Authorization extends \pl_authorization{}
+    class Address extends \pl_address{}
+    class OwnerAddress extends \pl_ownerAddress{}
+    class Buyer extends \pl_buyer{}
+    class Owner extends \pl_owner{}
+    class OrderDetails extends \pl_orderDetail{}
+    class Card extends \pl_card{}
+    class Transation extends \pl_transaction{}
+    class Result extends \pl_result{}
+    class Wallet extends \pl_wallet{}
+    class Recurring extends \pl_recurring{}
+    class BillingRecord extends \pl_billingRecord{}
+    class Authentication3DSecure extends \pl_authentication3DSecure{}
+    class BankAccountData extends \pl_bankAccountData{}
+    class Cheque extends \pl_cheque{}
+    class Creditor extends \pl_creditor{}
+}
+
+namespace Payline\Monext {
+    class PointOfSale extends \plbmnx_pointOfSale{}
+    class CustomPaymentPageCode extends \plbmnx_customPaymentPageCode{}
+    class CustomPaymentPageCodeList extends \plbmnx_customPaymentPageCodeList{}
+    class Contrat extends \plbmnx_contract{}
+    class Contrats extends \plbmnx_contracts{}
+    class CustomPaymentPageCode extends \plbmnx_customPaymentPageCode{}
+    class CustomPaymentPageCode extends \plbmnx_customPaymentPageCode{}
+    class CustomPaymentPageCode extends \plbmnx_customPaymentPageCode{}
+    */
+}
